f7bd6596afbd071035814fa7bccfd1403cd59553
Wu177777 <yangyang.wu@uqconnect.edu.au>
2022-12-29 13:49:01 +1000

    finish lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,26 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
+
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+    if (head == NULL) {
+        return 0;
+    }
+    node *tortoise=head;
+    node *hare=head;
+    while(1){
+        hare = hare->next;
+        if (hare == NULL) {
+            return 0;
+        }
+        hare = hare->next;
+        if (hare == NULL) {
+            return 0;
+        }
+        tortoise = tortoise->next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
+  
 }
